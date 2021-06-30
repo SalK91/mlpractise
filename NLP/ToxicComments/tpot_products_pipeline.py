fe6e95ee8f2940d04ea71f361e5316085fcff291
SalK91 <salmansaeedkhan91@yahoo.com>
2021-06-30 20:40:05 +0100

    Update

--- NLP/ToxicComments/tpot_products_pipeline.py
+++ NLP/ToxicComments/tpot_products_pipeline.py
@@ -1,9 +1,9 @@
 import numpy as np
 import pandas as pd
+from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import GaussianNB
 from sklearn.pipeline import make_pipeline, make_union
-from sklearn.svm import LinearSVC
-from sklearn.tree import DecisionTreeClassifier
 from tpot.builtins import StackingEstimator
 from tpot.export_utils import set_param_recursive
 
@@ -13,10 +13,10 @@ features = tpot_data.drop('target', axis=1)
 training_features, testing_features, training_target, testing_target = \
             train_test_split(features, tpot_data['target'], random_state=42)
 
-# Average CV score on the training set was: 0.8997750226253816
+# Average CV score on the training set was: 0.8998940894842418
 exported_pipeline = make_pipeline(
-    StackingEstimator(estimator=DecisionTreeClassifier(criterion="entropy", max_depth=10, min_samples_leaf=9, min_samples_split=13)),
-    LinearSVC(C=25.0, dual=False, loss="squared_hinge", penalty="l1", tol=0.001)
+    StackingEstimator(estimator=GaussianNB()),
+    LogisticRegression(C=15.0, dual=False, penalty="l2")
 )
 # Fix random state for all the steps in exported pipeline
 set_param_recursive(exported_pipeline.steps, 'random_state', 42)
