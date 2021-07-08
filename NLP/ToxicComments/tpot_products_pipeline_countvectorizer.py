f247a3e59b88b4b4789584874115f597c75c780b
SalK91 <salmansaeedkhan91@yahoo.com>
2021-07-08 22:29:53 +0100

    Update tpot_products_pipeline_countvectorizer.py

--- NLP/ToxicComments/tpot_products_pipeline_countvectorizer.py
+++ NLP/ToxicComments/tpot_products_pipeline_countvectorizer.py
@@ -1,11 +1,14 @@
+
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.svm import LinearSVC
 
+
 # NOTE: Make sure that the outcome column is labeled 'target' in the data file
-tpot_data = pd.read_csv('PATH/TO/DATA/FILE', sep='COLUMN_SEPARATOR', dtype=np.float64)
-features = tpot_data.drop('target', axis=1)
+
+#tpot_data = pd.read_csv('PATH/TO/DATA/FILE', sep='COLUMN_SEPARATOR', dtype=np.float64)
+#features = tpot_data.drop('target', axis=1)
 training_features, testing_features, training_target, testing_target = \
             train_test_split(features, tpot_data['target'], random_state=42)
 
