2556042a285b6b3c863181dbe9fa1c99301bf475
SalK91 <salmansaeedkhan91@yahoo.com>
2021-08-04 23:26:40 +0100

    Updates to image crop

--- ComputerVision/Yolo/opencv_image_yolo.py
+++ ComputerVision/Yolo/opencv_image_yolo.py
@@ -1,7 +1,6 @@
 ##https://www.pyimagesearch.com/2015/03/09/capturing-mouse-click-events-with-python-and-opencv/
 #pip3 install opencv-python
 
-
 # import the necessary packages
 import argparse
 import cv2
---
+++ ComputerVision/Yolo/yolo_crop.py
@@ -0,0 +1,177 @@
+# import the necessary packages
+import numpy as np
+import argparse
+import time
+import cv2
+import os
+
+
+## FUNCTIONS
+
+#CLICK CROP
+refPt = []
+cropping = False
+def click_and_crop(event, x, y, flags, param):
+	# grab references to the global variables
+	global refPt, cropping
+	# if the left mouse button was clicked, record the starting
+	# (x, y) coordinates and indicate that cropping is being
+	# performed
+	if event == cv2.EVENT_LBUTTONDOWN:
+		refPt = [(x, y)]
+		cropping = True
+	# check to see if the left mouse button was released
+	elif event == cv2.EVENT_LBUTTONUP:
+		# record the ending (x, y) coordinates and indicate that
+		# the cropping operation is finished
+		refPt.append((x, y))
+		cropping = False
+		# draw a rectangle around the region of interest
+		cv2.rectangle(image, refPt[0], refPt[1], (0, 255, 0), 2)
+		cv2.imshow("image", image)
+##################################################################################################################
+
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required=True,
+	help="path to input image")
+ap.add_argument("-y", "--yolo", required=True,
+	help="base path to YOLO directory")
+ap.add_argument("-c", "--confidence", type=float, default=0.5,
+	help="minimum probability to filter weak detections")
+ap.add_argument("-t", "--threshold", type=float, default=0.3,
+	help="threshold when applying non-maxima suppression")
+args = vars(ap.parse_args())
+
+# load the COCO class labels our YOLO model was trained on
+labelsPath = os.path.sep.join([args["yolo"], "coco.names"])
+LABELS = open(labelsPath).read().strip().split("\n")
+# initialize a list of colors to represent each possible class label
+np.random.seed(42)
+COLORS = np.random.randint(0, 255, size=(len(LABELS), 3),
+	dtype="uint8")
+
+# derive the paths to the YOLO weights and model configuration
+weightsPath = os.path.sep.join([args["yolo"], "yolov3.weights"])
+configPath = os.path.sep.join([args["yolo"], "yolov3.cfg"])
+# load our YOLO object detector trained on COCO dataset (80 classes)
+print("[INFO] loading YOLO from disk...")
+net = cv2.dnn.readNetFromDarknet(configPath, weightsPath)
+
+# load our input image and grab its spatial dimensions
+image = cv2.imread(args["image"])
+(H, W) = image.shape[:2]
+# determine only the *output* layer names that we need from YOLO
+ln = net.getLayerNames()
+ln = [ln[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+# construct a blob from the input image and then perform a forward
+# pass of the YOLO object detector, giving us our bounding boxes and
+# associated probabilities
+blob = cv2.dnn.blobFromImage(image, 1 / 255.0, (416, 416), swapRB=True, crop=False)
+net.setInput(blob)
+start = time.time()
+layerOutputs = net.forward(ln)
+end = time.time()
+# show timing information on YOLO
+print("[INFO] YOLO took {:.6f} seconds".format(end - start))
+
+
+# initialize our lists of detected bounding boxes, confidences, and
+# class IDs, respectively
+boxes = []
+confidences = []
+classIDs = []
+
+# loop over each of the layer outputs
+for output in layerOutputs:
+	# loop over each of the detections
+	for detection in output:
+		# extract the class ID and confidence (i.e., probability) of
+		# the current object detection
+		scores = detection[5:]
+		classID = np.argmax(scores)
+		confidence = scores[classID]
+		# filter out weak predictions by ensuring the detected
+		# probability is greater than the minimum probability
+		if confidence > args["confidence"]:
+			# scale the bounding box coordinates back relative to the
+			# size of the image, keeping in mind that YOLO actually
+			# returns the center (x, y)-coordinates of the bounding
+			# box followed by the boxes' width and height
+			box = detection[0:4] * np.array([W, H, W, H])
+			(centerX, centerY, width, height) = box.astype("int")
+			# use the center (x, y)-coordinates to derive the top and
+			# and left corner of the bounding box
+			x = int(centerX - (width / 2))
+			y = int(centerY - (height / 2))
+			# update our list of bounding box coordinates, confidences,
+			# and class IDs
+			boxes.append([x, y, int(width), int(height)])
+			confidences.append(float(confidence))
+			classIDs.append(classID)
+
+
+# apply non-maxima suppression to suppress weak, overlapping bounding
+# boxes
+idxs = cv2.dnn.NMSBoxes(boxes, confidences, args["confidence"],
+	args["threshold"])
+
+
+# ensure at least one detection exists
+if len(idxs) > 0:
+	# loop over the indexes we are keeping
+	for i in idxs.flatten():
+		# extract the bounding box coordinates
+		(x, y) = (boxes[i][0], boxes[i][1])
+		(w, h) = (boxes[i][2], boxes[i][3])
+		# draw a bounding box rectangle and label on the image
+		color = [int(c) for c in COLORS[classIDs[i]]]
+		cv2.rectangle(image, (x, y), (x + w, y + h), color, 2)
+		text = "{}: {:.4f}".format(LABELS[classIDs[i]], confidences[i])
+		cv2.putText(image, text, (x, y - 5), cv2.FONT_HERSHEY_SIMPLEX,
+			0.5, color, 2)
+# show the output image
+
+print('All Possible Bounding Boxes')
+print(boxes)
+print(classIDs)
+
+
+clone = image.copy()
+cv2.namedWindow("image")
+cv2.setMouseCallback("image", click_and_crop)
+# keep looping until the 'q' key is pressed
+while True:
+	# display the image and wait for a keypress
+	cv2.imshow("image", image)
+	key = cv2.waitKey(1) & 0xFF
+	# if the 'r' key is pressed, reset the cropping region
+	if key == ord("r"):
+		image = clone.copy()
+	# if the 'c' key is pressed, break from the loop
+	elif key == ord("c"):
+		break
+
+# if there are two reference points, then crop the region of interest
+# from the image and display it
+if len(refPt) == 2:
+	roi = clone[refPt[0][1]:refPt[1][1], refPt[0][0]:refPt[1][0]]
+	cv2.imshow("ROI", roi)
+
+	#print("[INFO] User Box Coodinates  x-axis {0} to {1} ".format(refPt[0][0], refPt[1][0]))
+	#print("[INFO] User Box Coodinates  y-axis {0} to {1} ".format(refPt[0][1], refPt[1][1]))
+	print("[INFO] User Box Coodinates (top-left corner, w,h)  [{0}, {1}, {2}, {3}] ".format(refPt[0][0],
+																					   refPt[0][1],
+																					   (refPt[1][0] - refPt[0][0]),
+																					   (refPt[1][1] - refPt[1][0])))
+
+
+
+	cv2.waitKey(0)
+
+
+# close all open windows
+cv2.destroyAllWindows()
+
+#python yolo_crop.py --image image_0.jpg --yolo yolo-coco
+
